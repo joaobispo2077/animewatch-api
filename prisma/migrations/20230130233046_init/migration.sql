1fa4de90c1d4ba0ec23da3aa8dc7210d075df963
João Bispo <joaobispo2077@gmail.com>
2023-01-30 20:36:47 -0300

    feat: setup database connection

---
+++ prisma/migrations/20230130233046_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Categories" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "link" TEXT,
+    "description" TEXT,
+    "description_link" TEXT
+);
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './shared/services/prisma.service';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
